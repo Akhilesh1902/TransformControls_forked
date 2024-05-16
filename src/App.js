72943bc089d6b0e7f6543995175a3930bf10a661
Akhilesh1902 <akhil1922002@gmail.com>
2024-05-16 17:34:19 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { Suspense, useState } from 'react'
+import { Suspense, useEffect, useState } from 'react'
 import { Canvas, useThree } from '@react-three/fiber'
 import { OrbitControls, TransformControls, ContactShadows, useGLTF, useCursor } from '@react-three/drei'
 import { proxy, useSnapshot } from 'valtio'
-
+import { Physics, useBox, usePlane, Debug } from '@react-three/cannon'
 // Reactive state model, using Valtio ...
 const modes = ['translate', 'rotate', 'scale']
 const state = proxy({ current: null, mode: 0 })
@@ -10,14 +10,34 @@ const state = proxy({ current: null, mode: 0 })
 function Model({ name, ...props }) {
   // Ties this component to the state model
   const snap = useSnapshot(state)
+  const [ref, api] = useBox(() => ({ mass: 1, position: [0, 5, 0], ...props, allowSleep: true }))
   // Fetching the GLTF, nodes is a collection of all the meshes
   // It's cached/memoized, it only gets loaded and parsed once
   const { nodes } = useGLTF('/compressed.glb')
   // Feed hover state into useCursor, which sets document.body.style.cursor to pointer|auto
   const [hovered, setHovered] = useState(false)
   useCursor(hovered)
+  const [_, sets] = useState()
+
+  useEffect(() => {
+    // console.log(snap.current)
+    if (props.enablePhysics) {
+      console.log(api)
+      // api.sleep()
+      api.wakeUp()
+    }
+    // api.wakeUp()
+
+    sets(Math.random())
+  }, [props.enablePhysics])
+
+  useEffect(() => {
+    api.sleep()
+  }, [])
+
   return (
     <mesh
+      // ref={ref}
       // Click sets the mesh as the new target
       onClick={(e) => (e.stopPropagation(), (state.current = name))}
       // If a click happened but this mesh wasn't hit we null out the target,
@@ -30,7 +50,7 @@ function Model({ name, ...props }) {
       name={name}
       geometry={nodes[name].geometry}
       material={nodes[name].material}
-      material-color={snap.current === name ? '#ff6080' : 'white'}
+      material-color={snap.current === name ? '#ff6080' : '#d5dea2'}
       {...props}
       dispose={null}
     />
@@ -51,26 +71,50 @@ function Controls() {
   )
 }
 
-export default function App() {
+function Plane(props) {
+  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }))
   return (
-    <Canvas camera={{ position: [0, -10, 80], fov: 50 }} dpr={[1, 2]}>
-      <pointLight position={[100, 100, 100]} intensity={0.8} />
-      <hemisphereLight color="#ffffff" groundColor="#b9b9b9" position={[-7, 25, 13]} intensity={0.85} />
-      <Suspense fallback={null}>
-        <group position={[0, 10, 0]}>
-          <Model name="Curly" position={[1, -11, -20]} rotation={[2, 0, -0]} />
-          <Model name="DNA" position={[20, 0, -17]} rotation={[1, 1, -2]} />
-          <Model name="Headphones" position={[20, 2, 4]} rotation={[1, 0, -1]} />
-          <Model name="Notebook" position={[-21, -15, -13]} rotation={[2, 0, 1]} />
-          <Model name="Rocket003" position={[18, 15, -25]} rotation={[1, 1, 0]} />
-          <Model name="Roundcube001" position={[-25, -4, 5]} rotation={[1, 0, 0]} scale={0.5} />
-          <Model name="Table" position={[1, -4, -28]} rotation={[1, 0, -1]} scale={0.5} />
-          <Model name="VR_Headset" position={[7, -15, 28]} rotation={[1, 0, -1]} scale={5} />
-          <Model name="Zeppelin" position={[-20, 10, 10]} rotation={[3, -1, 3]} scale={0.005} />
-          <ContactShadows rotation-x={Math.PI / 2} position={[0, -35, 0]} opacity={0.25} width={200} height={200} blur={1} far={50} />
-        </group>
-      </Suspense>
-      <Controls />
-    </Canvas>
+    <mesh ref={ref}>
+      <planeGeometry args={[100, 100]} />
+    </mesh>
+  )
+}
+
+export default function App() {
+  const [enablePhysics, setEnablePhysics] = useState(false)
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          setEnablePhysics((p) => !p)
+        }}>
+        enable physics
+      </button>
+      <Canvas camera={{ position: [0, -10, 80], fov: 50 }} dpr={[1, 2]}>
+        <pointLight position={[100, 100, 100]} intensity={0.8} />
+        {/* <hemisphereLight color="#ffffff" groundColor="#b9b9b9" position={[-7, 25, 13]} intensity={0.85} /> */}
+        <Suspense fallback={null}>
+          <Physics>
+            <Debug color="black" scale={1.1}>
+              <Plane position={[0, -20, 0]} />
+              {/* <group position={[0, 10, 0]}> */}
+              {/* <Model name="Curly" position={[1, -11, -20]} rotation={[2, 0, -0]} /> */}
+              {/* <Model name="DNA" position={[20, 0, -17]} rotation={[1, 1, -2]} /> */}
+              {/* <Model name="Headphones" position={[20, 2, 4]} rotation={[1, 0, -1]} /> */}
+              <Model enablePhysics={enablePhysics} name="Notebook" position={[0, 30, 0]} rotation={[Math.PI / 2, 0, 0]} scale={0.5} />
+              {/* <Model name="Rocket003" position={[18, 15, -25]} rotation={[1, 1, 0]} /> */}
+              {/* <Model name="Roundcube001" position={[-25, -4, 5]} rotation={[1, 0, 0]} scale={0.5} /> */}
+              <Model enablePhysics={enablePhysics} name="Table" position={[0, -18, 0]} rotation={[Math.PI / 2, 0, Math.PI / 2]} scale={1} />
+              {/* <Model name="VR_Headset" position={[7, -15, 28]} rotation={[1, 0, -1]} scale={5} /> */}
+              {/* <Model name="Zeppelin" position={[-20, 10, 10]} rotation={[3, -1, 3]} scale={0.005} /> */}
+              {/* <ContactShadows rotation-x={Math.PI / 2} position={[0, -35, 0]} opacity={0.25} width={200} height={200} blur={1} far={50} /> */}
+              {/* </group> */}
+            </Debug>
+          </Physics>
+        </Suspense>
+        <Controls />
+      </Canvas>
+    </>
   )
 }
